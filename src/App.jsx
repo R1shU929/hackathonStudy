35c76a8e87af840b5c413a6531127b82aa2981d8
배혜윤 <charlotte0209@naver.com>
2024-06-13 18:26:39 +0900

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import React from 'react';
+import styled from 'styled-components';
+
+let Bk = styled.div`
+  width: 50%;
+  height: 50%;
+  padding: 50%;
+  background-color: red;
+`;
 
 function App() {
-
   return (
-    <>
-    여기가 앱 페이지입니다람쥐
-    </>
-  )
+    <Bk>
+      Hello World!
+    </Bk>
+  );
 }
 
-export default App
+export default App;
+
