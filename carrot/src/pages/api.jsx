0a28db5c85e84675fc739e60555b85921fa6ebaf
배혜윤 <charlotte0209@naver.com>
2024-06-26 22:14:11 +0900

    api,usestate

--- carrot/src/pages/api.jsx
+++ carrot/src/pages/api.jsx
@@ -4,38 +4,53 @@ import axios from 'axios';
 const Api = () =>{
 
     const [posts,setPosts] = useState([]);
+    const [click,setClick] = useState(true);
+
+    const onClickButton1 = ()=>{
+        TestFunction(),
+        setClick(false)
+    }
+
+    const onClickButton2 = ()=>{
+        setPosts([]),
+        setClick(true)
+    }
 
     const TestFunction = async() => {
         try{
-            const response = await axios.get('https://jsonplaceholder.typicode.com/photos')
-            setPosts(response.data);
-        } 
-        
-        catch(error){
+            await axios.get('https://jsonplaceholder.typicode.com/photos')
+            .then(response=>{ // 통신하는 데이터명이 리스폰스가 댐
+                setPosts(response.data);
+        })
+    } 
+    catch(error){
             console.log(error)
         }
     }
-    
-    useEffect( ()=> {
-        TestFunction();
-        // axios({
-        //     method:'GET',
-        //     url:'https://jsonplaceholder.typicode.com/photos'
-        // }).then(response => setPosts(response.data))
-
-        // axios.get('https://jsonplaceholder.typicode.com/photos')
-        // .then(response => setPosts(response.data))
-    });
 
     return(
-    <ul>
-        {posts.map(post=>(
-            <li key={post.id}>
-                <div>{post.title}</div>
-                <div><img src={post.thumbnailUrl}/></div>
-                </li>
-        ))}
+        <div>
+            <div>
+        {click ? <button onClick={onClickButton1}>
+            통신하기
+        </button>
+        :
+        <button onClick={onClickButton2}>
+            통신한거 리-셋!
+        </button>
+}
+</div>
+        <ul>
+        {posts.length !==0 && posts.map(post => (
+                    <li key={post.id}>
+                        <div>{post.title}</div>
+                        <div>
+                            <img src={post.thumbnailUrl} />
+                        </div>
+                    </li>
+                ))}
     </ul>
+    </div>
     )
 }
 
